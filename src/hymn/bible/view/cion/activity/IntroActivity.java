cc13469616e1e4b6be8f53651b4c0e364d758615
cion49235 <cion49235@nate.com>
2018-04-02 10:37:50 +0900

    [20180402]광고제거

--- src/hymn/bible/view/cion/activity/IntroActivity.java
+++ src/hymn/bible/view/cion/activity/IntroActivity.java
@@ -40,7 +40,7 @@ public class IntroActivity extends Activity{
         setContentView(R.layout.activity_intro);
         context = this;
         retry_alert = true;
-        billing_process();//인앱정기결제체크
+//        billing_process();//인앱정기결제체크
         
         handler = new Handler();
         handler.postDelayed(runnable, 2000);
@@ -208,14 +208,18 @@ public class IntroActivity extends Activity{
     Runnable runnable = new Runnable() {
 		@Override
 		public void run() {
-			if(PreferenceUtil.getStringSharedData(context, PreferenceUtil.PREF_ISSUBSCRIBED, Const.isSubscribed).equals("true")) {
+			/*if(PreferenceUtil.getStringSharedData(context, PreferenceUtil.PREF_ISSUBSCRIBED, Const.isSubscribed).equals("true")) {
 				Intent intent = new Intent(context, MainFragmentActivity.class);
 				intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent);
 				finish();
 			}else {
 				show_inapp_alert();	
-			}
+			}*/
+			Intent intent = new Intent(context, MainFragmentActivity.class);
+			intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			finish();
 			//fade_animation
 			overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
 		}
